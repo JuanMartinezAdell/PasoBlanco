bac8b667ccf9d96e63ebec8bda12376ec0abf9b1
jumadell <jumadell@gmail.com>
2023-02-11 16:05:07 +0100

    Proyecto base

---
+++ resources/js/ComponentsNew/AuthenticationCardLogoVue.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Link :href="'/dashboard'">
+        <img width="60" height="60" :src="image" alt="Logo Paso">
+    </Link>
+</template>
+
+<script>
+    import image from '../../image/logo/logo.jpg'
+
+    export default {
+        data(){
+            return{
+                image:image
+            }
+        }
+    }
+</script>
